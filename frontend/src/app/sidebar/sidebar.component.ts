8f47ad79d14ccdf1b6588231b7d82e31b390953b
Alejo0508 <69872912+Alejo0508@users.noreply.github.com>
2021-03-11 10:14:52 -0500

    se modifica reservar, link de cuenta

--- frontend/src/app/sidebar/sidebar.component.ts
+++ frontend/src/app/sidebar/sidebar.component.ts
@@ -3,23 +3,23 @@ import { Location } from '@angular/common';
 
 declare const $: any;
 declare interface RouteInfo {
-    path: string;
-    title: string;
-    icon: string;
-    class: string;
+  path: string;
+  title: string;
+  icon: string;
+  class: string;
 }
 
 
 
 
 export const ROUTES: RouteInfo[] = [
-    { path: '/Carpool', title: 'Carpool',  icon: 'pe-7s-car', class: '' },
-    { path: '/usuario', title: 'Usuario',  icon:'pe-7s-id', class: '' },
-    { path: '/reservas', title: 'Tabla',  icon:'pe-7s-note2', class: '' },
-    { path: '/mapas', title: 'Mapas',  icon:'pe-7s-map-2', class: '' },
-    { path: '/notificacion', title: 'Notificaciones',  icon:'pe-7s-bell', class: '' },
-    { path: '/github', title: 'github',  icon:'pe-7s-share', class: '' },   
-   // { path: '/upgrade', title: 'Upgrade to PRO',  icon:'pe-7s-rocket', class: 'active-pro' },
+  { path: '/Carpool', title: 'Carpool', icon: 'pe-7s-car', class: '' },
+  { path: '/reservas', title: 'Reservas', icon: 'pe-7s-note2', class: '' },
+  { path: '/usuario', title: 'Usuario', icon: 'pe-7s-id', class: '' },
+  { path: '/mapas', title: 'Mapas', icon: 'pe-7s-map-2', class: '' },
+  // { path: '/notificacion', title: 'Notificaciones',  icon:'pe-7s-bell', class: '' },
+  { path: '/github', title: 'github', icon: 'pe-7s-share', class: '' },
+  // { path: '/upgrade', title: 'Upgrade to PRO',  icon:'pe-7s-rocket', class: 'active-pro' },
 ];
 
 @Component({
@@ -35,9 +35,9 @@ export class SidebarComponent implements OnInit {
     this.menuItems = ROUTES.filter(menuItem => menuItem);
   }
   isMobileMenu() {
-      if ($(window).width() > 991) {
-          return false;
-      }
-      return true;
+    if ($(window).width() > 991) {
+      return false;
+    }
+    return true;
   };
 }
