2bab7d2014ad6ca8a45345784c9a0fa58d9e7812
Alejo0508 <69872912+Alejo0508@users.noreply.github.com>
2021-03-11 17:26:20 -0500

    se termina de modificar formulario de usuarios

--- frontend/src/app/user/user.component.ts
+++ frontend/src/app/user/user.component.ts
@@ -5,11 +5,32 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
+
+
 export class UserComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
   }
+
+
+  showNotification(from, align){
+    const type = ['primary'];
+
+    var color = Math.floor((Math.random() * 4) + 1);
+    $.notify({
+        icon: "pe-7s-map-marker",
+        message: "Datos actualizados correctamente."
+    },{
+        type: type[color],
+        timer: 1000,
+        placement: {
+            from: from,
+            align: align
+        }
+    });
+}
+
 
 }
